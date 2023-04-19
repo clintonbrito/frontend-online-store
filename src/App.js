73793b77716a1b4fa94449a5f6bac6c94cd6a398
Clinton Brito <clintoncsb@gmail.com>
2023-04-19 16:43:02 -0300

    2o requisito ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá</h1>
+      <h1>Shopping Cart 🛒</h1>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+      </Switch>
     </div>
   );
 }
